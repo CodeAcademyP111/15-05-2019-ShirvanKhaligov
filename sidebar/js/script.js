d3f111b168809ce7aec4f376d86aa6178d326563
Şirvan <45492579+ShirvanKhaligov@users.noreply.github.com>
2019-05-15 23:33:32 +0400

    Add files via upload

---
+++ sidebar/js/script.js
@@ -0,0 +1,14 @@
+function functions(){
+    document.querySelector(".aside").style.marginLeft = "1px";
+    document.querySelector(".aside").style.transition = "3s";
+}
+function closing(){
+    document.querySelector(".aside").style.marginLeft="-250px";
+    document.querySelector(".aside").style.transition = "1s";
+}
+function closing2(){
+    document.querySelector(".aside").style.marginLeft="-350px";
+    document.querySelector(".aside").style.transition = "3s";
+    document.querySelector("body").style.backgroundColor="yellow";
+    document.querySelector("body").style.transition="1s";
+}
